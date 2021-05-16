98c244efd9d447c8082390a37ab4ab63e940b8c6
kavya259 <84277605+kavya259@users.noreply.github.com>
2021-05-16 21:07:57 +0530

    Add files via upload

---
+++ Assignment32/Assignment32/UserManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment32
+    {
+    class UserManager
+        {
+        public static void createUser(User user)
+            {
+            Console.WriteLine("Enter user id");
+            string id = Console.ReadLine();
+            int userid = 0;
+            try
+                {
+                userid = Int32.Parse(id);
+                }
+            catch(Exception ex)
+                {
+                Console.WriteLine(ex);
+
+                }
+
+            }
+        public static void showUserDetails(User user)
+            {
+
+            }
+
+        }
+    }
